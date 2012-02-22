d14e4b67a31112364f567d644972dbf61409ac26
Michael Brunton-Spall <michael@brunton-spall.co.uk>
2012-02-22 08:37:07 +0000

    Switch to use githubs http access

--- plugins/project/plugins.scala
+++ plugins/project/plugins.scala
@@ -3,6 +3,6 @@ import sbt._
 object Plugins extends Build {
   lazy val root = Project("global-plugins", file("."))
     .dependsOn(
-      uri("git://github.com/steppenwells/sbt-sh.git")
+      uri("http://github.com/steppenwells/sbt-sh.git")
     )
 }
